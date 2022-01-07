3519692a68c58148dbcba5e540faa88512dc159b
“Pedro <padronpedro@gmail.com>
2022-01-07 15:41:15 -0500

    first commit

---
+++ database/migrations/2022_01_07_203036_create_monsters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMonstersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('monsters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+            $table->string('ssn', 9);
+            $table->integer('eyes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('monsters');
+    }
+}
